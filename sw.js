5fec5ab0707109914d458d89208ccf20deb63791
fax0662294942-beep <fax0662294942@gmail.com>
2026-03-04 23:29:26 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,30 +1,31 @@
-const CACHE_NAME = 'ferie-permessi-v11';
-const BASE = '/ferie_e_permessi';
+const CACHE_NAME = 'ferie-permessi-v21';
 const ASSETS = [
-  BASE + '/index.html',
-  BASE + '/manifest.json',
-  BASE + '/icons/icon-192x192.png',
-  BASE + '/icons/icon-512x512.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
+    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
+      const resClone = res.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(e.request, resClone));
+      return res;
+    }).catch(() => caches.match('./index.html')))
   );
 });
