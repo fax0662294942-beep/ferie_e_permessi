6f4049fbcf2b5d310c1dfa75b9d0b2ceff931b8d
fax0662294942-beep <fax0662294942@gmail.com>
2026-07-06 16:10:17 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ferie-permessi-v22';
+const CACHE_NAME = 'ferie-permessi-v23';
 const ASSETS = ['index.html','manifest.json','icon-192x192.png','icon-512x512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); self.clients.claim(); });
